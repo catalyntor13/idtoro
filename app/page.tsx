0e3298223ddd9f6d6126458963d57ef2847fc7d0
Catalin Toro <108603533+catalyntor13@users.noreply.github.com>
2025-05-29 21:01:00 +0300

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+
+import Navbar from "./components/Header/Navbar1";
+import Hero1 from "./components/Hero/Hero1";
+import ContactForm from "./components/ContactForm/Contact1";
+import Footer from "./components/Footer/Footer1";
+
+export default function Home() {
+  return (
+   
+    <div>
+      <Navbar/>
+      <Hero1/>
+      <ContactForm/>
+      <Footer/>
+    </div>
+  );
+}
